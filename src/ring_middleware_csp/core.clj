9aa7242824cd5631e2bddda7a2a0aba9f2dae610
egs33 <egs33.bicycle@gmail.com>
2020-07-27 12:18:40 +0900

    Add simple function

--- src/ring_middleware_csp/core.clj
+++ src/ring_middleware_csp/core.clj
@@ -1,1 +1,32 @@
-(ns ring-middleware-csp.core)
+(ns ring-middleware-csp.core
+  (:require
+   [clojure.string :as str]))
+
+(defn- value->str [value]
+  (->> (if (coll? value) value [value])
+       (map #(if (keyword? %)
+               (str "'" (name %) "'")
+               %))
+       (str/join " ")))
+
+(defn compose
+  "Make string value for CSP header from policy map"
+  [policy]
+  (->> policy
+       (map (fn [[d v]] (str (name d) " " (value->str v))))
+       (str/join ";")))
+
+(defn wrap-csp
+  "Middleware that adds Content-Security-Policy header.
+  Accepts the following options:
+  :policy          - CSP in {directive-name directive-values} format. See README for details.
+  :report-only?    - true if Use Content-Security-Policy-Report-Only Header."
+  [handler {:keys [policy report-only?]}]
+  (let [header-name (if report-only?
+                      "Content-Security-Policy-Report-Only"
+                      "Content-Security-Policy")
+        header-value (compose policy)]
+    (fn [req]
+      (let [res (handler req)]
+        (assoc-in res [:headers header-name] header-value)))))
+
--- test/ring_middleware_csp/core_test.clj
+++ test/ring_middleware_csp/core_test.clj
@@ -1,3 +1,24 @@
 (ns ring-middleware-csp.core-test
-  (:require [clojure.test :refer :all]
-            [ring-middleware-csp.core :refer :all]))
+  (:require
+   [clojure.test :refer :all]
+   [ring-middleware-csp.core :refer :all]))
+
+(deftest compose-test
+  (is (= "" (compose {})))
+  (is (= "default-src 'self'" (compose {:default-src :self})))
+  (is (= "default-src 'self'" (compose {:default-src [:self]})))
+  (is (= "default-src 'self' https://example.com;connect-src 'none';script-src 'self' https://script.example.com;style-src 'unsafe-inline' https://style.example.com"
+         (compose {:default-src [:self "https://example.com"]
+                   :connect-src :none
+                   :script-src [:self "https://script.example.com"]
+                   :style-src [:unsafe-inline "https://style.example.com"]}))))
+
+(deftest wrap-csp-test
+  (let [handler (constantly {:status 200 :headers {} :body ""})]
+    (is (= "default-src 'self'"
+           (get-in ((wrap-csp handler {:policy {:default-src :self}}) {})
+                   [:headers "Content-Security-Policy"])))
+    (is (= "default-src 'self'"
+           (get-in ((wrap-csp handler {:policy {:default-src :self}
+                                       :report-only? true}) {})
+                   [:headers "Content-Security-Policy-Report-Only"])))))
