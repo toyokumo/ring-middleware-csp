b112777229f07d84da6bacb589bd5382ed8d4aa8
egs33 <egs33.bicycle@gmail.com>
2020-12-17 15:23:00 +0900

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject toyokumo/ring-middleware-csp "0.1.1-SNAPSHOT"
+(defproject toyokumo/ring-middleware-csp "0.1.1"
   :description "Ring middleware for Content Security Policy"
   :url "https://github.com/toyokumo/ring-middleware-csp"
   :license {:name "Apache, Version 2.0"
