be4032d2b5e979c62ad4aafe47c4c43a24043a0f
egs33 <egs33.bicycle@gmail.com>
2020-12-24 16:46:59 +0900

    Bump minor version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject toyokumo/ring-middleware-csp "0.1.2-SNAPSHOT"
+(defproject toyokumo/ring-middleware-csp "0.2.0-SNAPSHOT"
   :description "Ring middleware for Content Security Policy"
   :url "https://github.com/toyokumo/ring-middleware-csp"
   :license {:name "Apache, Version 2.0"
